c296b1656a71e3e4c631d4c21e78a69876afe31f
matata86 <ma.prochazka@gmail.com>
2026-09-26 08:33:28 +0200

    Zdroj hodnocení (ratingSource): TMDB, IMDb (Luna, Cinemeta) nebo Sosáč

--- tests/test_enrich_tmdb.py
+++ tests/test_enrich_tmdb.py
@@ -6,7 +6,7 @@ from nokturno_core.lib import enrich
 
 class FakeTmdb:
     def brief(self, ctype, imdb):
-        return {"description": "Český popis", "imdbRating": 7.1, "genres": ["Drama"], "poster": "", "year": "2020"}
+        return {"description": "Český popis", "imdbRating": 7.1, "ratingSource": "tmdb", "genres": ["Drama"], "poster": "", "year": "2020"}
 
 
 class TestEnrichTmdb(unittest.TestCase):
@@ -25,6 +25,15 @@ class TestEnrichTmdb(unittest.TestCase):
         with mock.patch.object(enrich, "_cinemeta", return_value={"description": "English", "imdbRating": 6}):
             enrich.enrich(metas, deadline=5)
         self.assertEqual(metas[0]["description"], "English")
+        self.assertEqual(metas[0]["ratingSource"], "imdb")
+
+    def test_zdroj_hodnoceni_jen_s_hodnocenim(self):
+        # titul ze Sosáče má vlastní hodnocení; TMDB doplní popis, ale značku nepřepíše
+        metas = [{"name": "Duha", "imdb_id": "tt0000003", "imdbRating": 8, "ratingSource": "sosac"},
+                 {"name": "Duha", "imdb_id": "tt0000004"}]
+        enrich.enrich(metas, tmdb=FakeTmdb(), deadline=5)
+        self.assertEqual((metas[0]["imdbRating"], metas[0]["ratingSource"]), (8, "sosac"))
+        self.assertEqual((metas[1]["imdbRating"], metas[1]["ratingSource"]), (7.1, "tmdb"))
 
 
 if __name__ == "__main__":
